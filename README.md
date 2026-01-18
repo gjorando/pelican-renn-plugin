05029ecf0db291230457edc888b559865e58f0b1
gjorando <guillaume.jorandon.1@ulaval.ca>
2026-01-18 21:08:12 +0100

    Added the hidden category functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pelican-renn: A Plugin for Pelican
 
-A plugin for my own needs
+A plugin for my own needs.
 
 ## Installation
 
@@ -21,46 +21,75 @@ PLUGINS = ["pelican_renn_plugin", "..."]
 
 ## Usage
 
-### The "projects" reST directive
+### The `projects` and `project` directives
 
-A grid of project tiles with a pop-up element.
-
-* _Directive Type:_ "projects"
-* _Directive Arguments:_ n.a.
-* _Directive Options:_ n.a.
-* _Directive Content:_ one or more "project" directives (one per project tile), and arbitrary content that accompanies the project grid.
+A `projects`directive is a grid of project tiles with a pop-up element.
 
 Example usage:
 
 ```rest
-.. project::
+.. projects::
 
-    (One or more "project" directives, see below)
+    .. project::
+        :title: My project
+        :image: images/my_project.jpg
+        :links: Website <https://perdu.com>, http://other-link.net
+
+        This is a description for my project.
+
+    .. project::
+        :title: My other project
+        :image: images/my_other_project.jpg
+
+        This is a description for my other project.
 
     This is arbitrary content that goes alongside the project grid. It will always be inserted after it.
 
     Another paragraph!
 ```
 
-### The "project" reST directive
+#### `projects`
 
-A single element of the "projects" grid.
+* _Directive Type:_ `projects`
+* _Directive Arguments:_ n.a.
+* _Directive Options:_ n.a.
+* _Directive Content:_ one or more `project` directives (one per project tile), and arbitrary content that accompanies the project grid.
 
-* _Directive Type:_ "project"
+#### `project`
+
+* _Directive Type:_ `project`
 * _Directive Arguments:_ n.a.
 * _Directive Options:_
-  * _"title":_ name of the project (mandatory)
-  * _"image":_ background image for the tile (mandatory)
-  * _"links":_ optional list of links added in the pop-up (optional, see example below).
+  * `title`: name of the project (mandatory)
+  * `image`: background image for the tile (mandatory)
+  * `links`: optional list of links added in the pop-up (optional, see example below).
 * _Directive Content:_ the content of the pop-up.
 
-Example usage:
+### Hidden categories
 
-```rest
-.. project::
-    :title: My project
-    :image: images/my_project.jpg
-    :links: Website <https://perdu.com>, http://other-link.net
+This plugin implements "virtual" categories for each "base" category with at least one hidden article. We call this hidden category a "virtual" category, because it is not listed in the category list page. It is merely a view of the base category which includes articles with a status set to `hidden` or `published`. A number of settings can be set within `pelicanconf.py`.
 
-    This is a description for my project.
-```
+#### `HIDDENCATEGORY_ENABLE`
+
+A flag that enables the feature. The default is `False`.
+
+#### `HIDDENCATEGORY_NAME`
+
+The name of the virtual category. `base_category` is available as a template parameter to expose the base category object. The default is `{base_category}-full` (the string representation of `Category` objects is the category name).
+
+#### `HIDDENCATEGORY_URL`
+
+The URL to use for a virtual category. The default is computed from `CATEGORY_URL`, adding `-full` to the last folder.
+
+#### `HIDDENCATEGORY_SAVE_AS`
+
+The location to save a virtual category. The default is computed from `HIDDENCATEGORY_URL`, adding an `index.html` if
+the URL doesn't specify a filename.
+
+#### `HIDDENCATEGORY_OVERRIDES`
+
+Dictionary mapping a base category slug to a dictionary of setting overrides. These overrides update the `settings` attribute of a category, while leaving the original settings untouched. The default is `{}`.
+
+#### `HIDDENCATEGORY_EXCLUDES`
+
+List of category slugs to ignore in the creation of the hidden categories. The default is `[]`.
---
+++ pelican/plugins/pelican_renn_plugin/hidden_category.py
@@ -0,0 +1,111 @@
+import os
+
+from collections import defaultdict
+from operator import attrgetter
+
+from pelican.contents import Category
+
+
+class HiddenCategory(Category):
+    """
+    Virtual categories that are meant to list all the articles from the base category,
+    including hidden articles. Note that all articles retain their original category,
+    hence why we call them "virtual" categories.
+
+    To ensure they're treated like the base category, the slug is set to that of the
+    base category. This may or may not have unintended side effects...?
+    """
+
+    def __init__(self, base_category):
+        # Optional settings overrides
+        settings = base_category.settings.copy()
+        settings.update(
+            base_category.settings["HIDDENCATEGORY_OVERRIDES"].get(
+                base_category.slug,
+                dict()
+            )
+        )
+
+        # Parse the name of the hidden category
+        name = settings[f"{self.__class__.__name__.upper()}_NAME"]
+
+        super().__init__(name.format(base_category=base_category),
+                         settings)
+
+        # Force the slug of the hidden category to be the same as that of the base's
+        self.slug = base_category.slug
+
+        # The base category is stored here
+        self.base_category = base_category
+
+
+def create_hidden_categories(generator):
+    """
+    Signal that creates our hidden categories.
+
+    :param generator: Generator instance.
+    """
+
+    # Do nothing if the functionality is not enable
+    if not generator.settings["HIDDENCATEGORY_ENABLE"]:
+        return
+
+    extra_categories = defaultdict(list)
+    for article in generator.hidden_articles:
+        category_name = article.category.name if article.category else None
+        # Skip articles without a category, or excluded categories
+        if (not category_name
+            or article.category.slug in generator.settings["HIDDENCATEGORY_EXCLUDES"]):
+            continue
+
+        extra_categories[category_name].append(article)
+
+    # We also add the visible articles
+    for article in generator.articles:
+        category_name = article.category.name if article.category else None
+        # Skip articles without a category
+        if not category_name:
+            continue
+
+        # Categories that don't have hidden articles won't generate a hidden category
+        if category_name in extra_categories:
+            extra_categories[category_name].append(article)
+
+    # We copy the original list because we want it to be distinct from
+    # generator.context["categories"], which keeps the new category hidden from the
+    # categories template page
+    generator.categories = generator.categories.copy()
+    for category_name, articles in extra_categories.items():
+        category = HiddenCategory(articles[0].category)
+        articles.sort(key=attrgetter("date"), reverse=True)
+        generator.categories.append((category, articles))
+
+
+def set_default_settings(instance):
+    """
+    Signal that initializes our plugin default settings.
+
+    :param instance: The Pelican instance.
+    """
+
+    instance.settings.setdefault("HIDDENCATEGORY_ENABLE", False)
+
+    instance.settings.setdefault("HIDDENCATEGORY_NAME", "{base_category}-full")
+
+    url_parent, url_name = os.path.split(instance.settings["CATEGORY_URL"])
+    instance.settings.setdefault(
+        "HIDDENCATEGORY_URL",
+        f"{url_parent}-full/{url_name}"
+    )
+
+    sa_parent, sa_name = os.path.split(instance.settings["HIDDENCATEGORY_URL"])
+    instance.settings.setdefault(
+        "HIDDENCATEGORY_SAVE_AS",
+        f"{sa_parent}/{sa_name if sa_name else "index.html"}"
+    )
+
+    print(instance.settings["HIDDENCATEGORY_URL"], instance.settings["HIDDENCATEGORY_SAVE_AS"])
+
+    instance.settings.setdefault("HIDDENCATEGORY_OVERRIDES", dict())
+
+    instance.settings.setdefault("HIDDENCATEGORY_EXCLUDES", [])
